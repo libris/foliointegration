1cd7cd6d2d52d649b242fe0f3c954e795f4414dd
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2025-06-12 12:57:03 +0200

    wip

--- src/main/java/se/kb/libris/foliointegration/FolioSync.java
+++ src/main/java/se/kb/libris/foliointegration/FolioSync.java
@@ -1,18 +1,19 @@
 package se.kb.libris.foliointegration;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FolioSync {
 
     public final static String SYNCED_UNTIL_KEY = "FOLIOSyncStateSyncedUntil";
 
-    public static void run() throws SQLException {
+    private final static List<String> SIGEL_LIST = Collections.unmodifiableList( Arrays.asList(System.getenv("SIGEL").split(",")) );
+
+    public static void run() throws SQLException, IOException {
         Connection connection = Storage.getConnection();
         long syncedUntil = Long.parseLong( Storage.getState(SYNCED_UNTIL_KEY, connection) );
 
@@ -36,26 +37,7 @@ public class FolioSync {
         for (int i = 0; i < ids.size(); ++i) {
             long id = ids.get(i);
             String data = datas.get(i);
-            long checksum = calculateCheckSum(data);
-
-            try (PreparedStatement statement = connection.prepareStatement("SELECT checksum FROM exported_checksum WHERE entity_id = ?")) {
-                statement.setLong(1, id);
-                statement.execute();
-                try (ResultSet resultSet = statement.getResultSet()) {
-                    if (resultSet.next()) {
-                        long lastExportedChecksum = resultSet.getLong(1);
-                        if (lastExportedChecksum != checksum) {
-                            // A visible change. Write it to folio.
-                        }
-                    }
-                }
-            }
-
-            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO exported_checksum(entity_id, checksum) VALUES(?, ?) ON CONFLICT(entity_id) DO UPDATE SET checksum=excluded.checksum")) {
-                statement.setLong(1, id);
-                statement.setLong(2, checksum);
-                statement.execute();
-            }
+            considerForExport(id, data, connection);
         }
 
         // Commit state for next pass
@@ -66,6 +48,52 @@ public class FolioSync {
         connection.commit();
     }
 
+    private static void considerForExport(long id, String data, Connection connection) throws SQLException, IOException {
+        Map mainEntity = Storage.mapper.readValue(data, Map.class);
+
+        // Is this a folio root record (an 'Item' held by a selected library)?
+        boolean isRootRecord = false; // assumption
+        if (mainEntity.get("@type") instanceof String type) {
+            if (type.equals("Item")) {
+                if (mainEntity.get("heldBy") instanceof Map heldBy) {
+                    if (heldBy.get("@id") instanceof String heldById) {
+                        String libraryCode = heldById.substring(heldById.lastIndexOf('/') + 1);
+                        isRootRecord |= SIGEL_LIST.contains(libraryCode);
+                    }
+                }
+            }
+        }
+
+        if (isRootRecord) { // If this *is* a root record, we may need to write it to folio.
+
+            long checksum = calculateCheckSum(data);
+            boolean exported = false; // assumption
+            try (PreparedStatement statement = connection.prepareStatement("SELECT checksum FROM exported_checksum WHERE entity_id = ?")) {
+                statement.setLong(1, id);
+                statement.execute();
+                try (ResultSet resultSet = statement.getResultSet()) {
+                    if (resultSet.next()) {
+                        long lastExportedChecksum = resultSet.getLong(1);
+                        if (lastExportedChecksum != checksum) {
+                            // A visible difference. Write it to folio!
+
+                            exported = true;
+                        }
+                    }
+                }
+            }
+            if (exported) {
+                try (PreparedStatement statement = connection.prepareStatement("INSERT INTO exported_checksum(entity_id, checksum) VALUES(?, ?) ON CONFLICT(entity_id) DO UPDATE SET checksum=excluded.checksum")) {
+                    statement.setLong(1, id);
+                    statement.setLong(2, checksum);
+                    statement.execute();
+                }
+            }
+
+        } else { // Could it have affected another record that is a root record ?
+            // TODO
+        }
+    }
 
     private static long calculateCheckSum(Object data) {
         return calculateCheckSumInternal(data, 1, 1);

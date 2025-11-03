764884580c848fd45c081c3b74146b47ac27d604
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2025-11-03 16:12:16 +0100

    Moving towards JSLT conversion.

--- src/main/java/se/kb/libris/foliointegration/Format.java
+++ src/main/java/se/kb/libris/foliointegration/Format.java
@@ -122,11 +122,14 @@ public class Format {
                 """);
 
         Expression holdingsJSLT = Parser.compileString("""
-                [for (.)
-                    {
-                        "source" : "LIBRIS",
-                        "hrid" : .itemOf.meta.controlNumber + "-" + 1
-                    }
+                let root = (.)
+                
+                [
+                    for ( zip-with-index(.hasComponent) ) if ( not ( contains( "BESTÄLLD", .shelfMark.label ) ) )
+                        {
+                            "hrid" : $root.itemOf.meta.controlNumber + "-" + .index,
+                            "shelfMark" : .value.shelfMark
+                        }
                 ]
                 """);
 
@@ -137,57 +140,23 @@ public class Format {
         Map jsltModifiedInstance = Storage.mapper.treeToValue(instanceJsonNodeTransformed, Map.class);
         doTypeLookup(jsltModifiedInstance);
 
-        Storage.log(" ** CONVERTED INTO: " + jsltModifiedInstance);
-
+        //Storage.log(" ** CONVERTED INTO: " + jsltModifiedInstance);
 
         List<Map> allItems = getItems( (String) originalMainEntity.get("@id"), connection);
+        List folioItems = null;
         for (Map item : allItems) {
             // embed the instance, to make instance-info available during JSLT-transform.
             item.put("itemOf", originalMainEntity);
+
+            JsonNode holdingsJsonNodeOriginal = Storage.mapper.valueToTree(item);
+            JsonNode holdingsJsonNodeTransformed = holdingsJSLT.apply(holdingsJsonNodeOriginal);
+            folioItems = Storage.mapper.treeToValue(holdingsJsonNodeTransformed, List.class);
         }
 
-        JsonNode holdingsJsonNodeOriginal = Storage.mapper.valueToTree(allItems);
-        JsonNode holdingsJsonNodeTransformed = holdingsJSLT.apply(holdingsJsonNodeOriginal);
-        List jsltModifiedHoldings = Storage.mapper.treeToValue(holdingsJsonNodeTransformed, List.class);
-        Storage.log(" ** CONVERTED HOLDINGS INTO: " + jsltModifiedHoldings);
 
-        /*
-        List<Map> holdingRecords = new ArrayList<>();
-        for (Map item : allItems) {
-            String holdingHrid =  item.get("@id") + (String)((Map) item.get("heldBy")).get("@id"); // TEMP
-
-            List folioItems = new ArrayList<>();
-            if (item.containsKey("hasComponent")) {
-                for (Map component : (List<Map>) item.get("hasComponent")) {
-                    folioItems.add(Map.of("hrid", "" + item.get("@id") + component.hashCode() ));
-                }
-            } else {
-                folioItems.add(Map.of("hrid", item.get("@id")));
-            }
-
-            Map holdingRecord = Map.of(
-                    "permanentLocationId", "57246b20-76a7-4725-91c7-dea9f336de4f", // TEMP MAKE CONFIGURABLE
-                    //"sourceId", "912ecb39-c577-4596-ad4b-0ed8dedc3a33", // TEMP
-                    "sourceId", System.getenv("SOURCE_GUID"),
-                    "hrid", holdingHrid,
-                    "items", folioItems);
-            holdingRecords.add(holdingRecord);
-        }*/
-
-
-        /*
-        Map instance = new HashMap();
-        instance.put("source", "LIBRIS");
-        instance.put("hrid", hrid);
-        instance.put("instanceTypeId", instanceTypeId);
-        instance.put("title", mainTitle);
-        instance.put("indexTitle", mainTitle);
-        instance.put("sourceUri", originalMainEntity.get("@id"));*/
         Map converted = new HashMap();
-
         converted.put("instance", jsltModifiedInstance);
-        //converted.put("holdingsRecords", holdingRecords);
-        converted.put("holdingsRecords", new ArrayList<Map>()); // TEMP
+        converted.put("holdingsRecords", folioItems);
 
         return converted;
     }

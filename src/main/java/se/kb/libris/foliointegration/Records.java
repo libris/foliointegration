f937e53647c8a56451669d866587d6fbef26c9ce
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2025-11-27 11:27:01 +0100

    Add contribution to embellish list.

--- src/main/java/se/kb/libris/foliointegration/Records.java
+++ src/main/java/se/kb/libris/foliointegration/Records.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 import java.util.*;
 
 public class Records {
-    private final static List<String> propertiesOfInterest = Arrays.asList("mainEntity", "instanceOf", "itemOf", "subject", "agent");
+    private final static List<String> propertiesOfInterest = Arrays.asList("mainEntity", "instanceOf", "itemOf", "subject", "agent", "contribution");
 
     /**
      * Import a fetched record. The connection will be written to, but not
